e7394f0d22b0472c161b0d9c0ff6b65a3c59d62c
Aurélien Pautet <aurelien.pautet@imt-atlantique.net>
2025-11-26 12:50:05 +0100

    Initial Commit

---
+++ AI Corrector Swift/AI_Corrector_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Corrector_SwiftApp.swift
+//  AI Corrector Swift
+//
+//  Created by Aurélien Pautet on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AI_Corrector_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Corrector Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Corrector Swift
+//
+//  Created by Aurélien Pautet on 26/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
